8cefcb1d3bef9a69a53c07baec3f5a42946a831f
sravan969 <sravan.mghs@gmail.com>
2019-07-28 11:17:09 +0530

    1st commit

---
+++ src/test/java/sravan1/sravantest.java
@@ -0,0 +1,17 @@
+package sravan1;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class sravantest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}
